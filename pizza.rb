4267101e43ee2a05b765b4e28481138959314332
Hubert Nguyen <hubertanguyen@gmail.com>
2014-03-06 11:02:14 -0600

    passed first test, topping name

--- pizza.rb
+++ pizza.rb
@@ -2,4 +2,11 @@ class Pizza
 end
 
 class Topping
+  attr_reader :name
+  #Instantiate a new topping.
+  #name -The string name of the topping
+  #REturna new topping
+  def initialize(name)
+    @name = name
+  end
 end
